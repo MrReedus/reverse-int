72adddf00f71503925c9b0e5284fc2b57247671f
Evgenii <naterid@yandex.ru>
2023-01-17 01:34:39 +0700

    to perfom task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    const toString = n + '';
+    if (toString[toString.length - 1] === 0) {
+        toString.replace('0', '')
+    }
+    return +toString.split('')
+        .reverse()
+        .join('');
 }
+
